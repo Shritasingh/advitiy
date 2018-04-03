05e48c50a8adbf092f294e793a271f4a9c2f4f40
Shritasingh <37498340+Shritasingh@users.noreply.github.com>
2018-04-03 22:55:20 +0530

    Update controller1.py

--- controller1.py
+++ controller1.py
@@ -1,39 +1,44 @@
 import numpy as np
 from pylab import *
 
+
+def controlLaw (q_bo, v_w_bo_b_m, v_b_b_m, dt):
 '''
+the function takes input: 
+normalised quaternion rotating a vector from body frame to orbit frame
+angular velocity of body frame wrt to orbit frame in body frame
+measured magnetic field in body frame
+gives output:
+current to be applied in magnetorquers
+
 here:
 q_BO = [qs qv(in body frame)]
 dt is time step
-W,B are in body frame
-
-the code assumes: 
 q_BO is normalised
-magnetorquers are perfectly aligned with body frame axes
+assumed that magnetorquers are perfectly aligned with body frame axes
 '''
 
-def controlLaw (q_BO, v_Wb_m, v_Bb_m, dt):
-	N=[1,1,1]                                 #[number of turns of magnetorquer aligned with x axis of body frame, " y axis ", " z axis "]
+    N=[1,1,1]                                 #[number of turns of magnetorquer aligned with x axis of body frame, " y axis ", " z axis "]
     A=[1,1,1]                                 #[area of magnetorquer aligned with x axis of body frame, " y axis ", " z axis "]
-    Kp = np.array([[ -1,  0,  0], 
+    m_Kp = np.array([[ -1,  0,  0], 
                    [  0, -1,  0], 
                    [  0,  0, -1]])
-    Ki = np.array([[ -1,  0,  0], 
+    m_Ki = np.array([[ -1,  0,  0], 
                    [  0, -1,  0], 
                    [  0,  0, -1]])
-    Kd = np.array([[ -1,  0,  0], 
+    m_Kd = np.array([[ -1,  0,  0], 
                    [  0, -1,  0], 
                    [  0,  0, -1]])
-    v_e=np.array([])                          #error term in body frame (sin(theta)*UnitVectorOfAxisOfRotation)
-    v_ie=np.array([0,0,0])                    #integral term
-    Bmodsq = (v_Bb_m[0])**2 + (v_Bb_m[1])**2 + (v_Bb_m[2])**2 
+    v_e_b=np.array([])                          #error term in body frame (sin(theta)*UnitVectorOfAxisOfRotation)
+    v_ie_b=np.array([0,0,0])                    #integral term
+    Bmodsq=(v_b_b_m[0])**2 + (v_b_b_m[1])**2 + (v_b_b_m[2])**2 
     for i in range (0,3,1):
-        e=np.append(e,[2*q[0]*q[i+1]])
-        ie[i]=ie[i]+e[i]*dt
-    v_m = (Kp.dot(v_e)+Ki.dot(v_ie)+Kd.dot(v_Wb_m))/Bmodsq
-    v_mControl = np.cross(m,B)                #magnetic moment to be applied
-    I=(v_mControl/A)/N                        #[current to be applied in magnetorquer aligned with x axis of body frame," y axis ", " z axis "]
-    return I
+        v_e_b=np.append(v_e_b,[2*q_bo[0]*q_bo[i+1]])
+        v_ie_b[i]=v_ie_b[i]+v_e_b[i]*dt
+    v_m=(Kp.dot(v_e_b)+Ki.dot(v_ie_b)+Kd.dot(v_w_bo_b_m))/Bmodsq   #PID control 
+    v_mControl=np.cross(v_m,v_b_b_m)                #magnetic moment to be applied
+    v_I=(v_mControl/A)/N                            #[current to be applied in magnetorquer aligned with x axis of body frame," y axis ", " z axis "]
+    return v_I
 #controlLaw([1,1,1,1],[0,0,0],[-1,0,1],1)
 
        
