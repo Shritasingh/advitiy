3b4d3226dacd75137ba986801a6ad20aa234cfd0
Shritasingh <37498340+Shritasingh@users.noreply.github.com>
2018-07-13 13:58:38 +0530

    Add files via upload

---
+++ power.py
@@ -0,0 +1,53 @@
+import numpy as np
+import math
+import qnv as qnv
+import frames as fs
+import constants_1U as constants
+from satellite import Satellite
+
+'''
+this function takes input:
+    satellite class
+    (sun vector in body frame)
+             gives output:
+    power received by solar panels
+'''    
+        
+def power(sat):
+    v_S1  = constants.v_Ax                                   #area in metre square
+    v_S2  = (-1)*constants.v_Ax
+    v_S3  = constants.v_Ay
+    v_S4  = (-1)*constants.v_Ay
+    v_S5  = constants.v_Az
+    v_S6  = (-1)*constants.v_Az
+    area_vector = np.array([v_S1,v_S2,v_S3,v_S4,v_S5,v_S6])
+    area = np.array([v_S1,(-1)*v_S2,v_S3,(-1)*v_S4,v_S5,(-1)*v_S6])
+    v_S1_norm = np.linalg.norm(v_S1) 
+    face_vector = area_vector/(v_S1_norm)
+    EFFICIENCY = 0.27
+    INTENSITY = 1346                                         #in watts per metre square
+    MIN_ANGLE = 90
+    MIN_VAL = math.cos(math.radians(MIN_ANGLE))
+    v_sun_b_m = sat.getSun_b_m()
+    #v_sun_b_m = np.array([1,1,1])
+    cos_values = np.array([])
+    for i in range (0,6):
+            cos_values = np.append(cos_values,[np.dot(face_vector[i],v_sun_b_m)])
+    LIGHT=sat.getLight()
+    for i in range (0,6):
+        if (cos_values[i]<MIN_VAL):
+            cos_values[i]=0
+    
+    effective_area=np.array([])
+    for i in range (0,6):
+        effective_area=np.append(effective_area,[(0.0064)*cos_values[i]])    #area of each side is 0.01 msq
+    v_POWER= np.array([])
+    POWER=0
+    for i in range (0,6):
+        v_POWER = np.append(v_POWER,[EFFICIENCY*INTENSITY*effective_area[i]])
+        POWER=POWER+EFFICIENCY*INTENSITY*effective_area[i]
+    if (LIGHT==0 or LIGHT==0.5):
+        v_POWER=np.array([0,0,0,0,0,0])
+        POWER=0
+    v_POWER=np.append(v_POWER,[POWER])
+    return v_POWER
---
+++ power_sim.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import qnv as qnv
+import frames as f
+import constants_1U as constants
+import satellite as sat 
+import power as pow
+
+sgp_output = np.genfromtxt('sgp_output.csv', delimiter=",")
+si_output = np.genfromtxt('si_output.csv', delimiter=",")
+light_output = np.genfromtxt('light_output.csv', delimiter=",")
+T = si_output[:,0] #storing first element as time
+energy=0
+N = len(T)
+power_output = np.zeros((N,9))
+
+state=np.array([1,0,0,0,0,0,0])
+
+#Make satellite object
+advitiy = sat.Satellite(state,0)
+
+for i in range(N):
+    #setting time
+    advitiy.setTime(T[i])
+    dt = T[1] - T[0]
+    
+    #getting light, sun vector, position, velocity from csv files
+    light = light_output[i,1]
+    v_sun_i = si_output[i,1:4].copy()  
+    v_pos_i = sgp_output[i,1:4].copy()
+    v_vel_i = sgp_output[i,4:7].copy()
+    
+    #calculating sun vector body and orbit frame
+    v_sun_o = f.ecif2orbit(v_pos_i,v_vel_i,v_sun_i)
+    v_sun_b = v_sun_o.copy()
+    
+    #setting attributes to satellite
+    advitiy.setPos(v_pos_i)
+    advitiy.setVel(v_vel_i)
+    advitiy.setSun_i(v_sun_i)
+    #advitiy.setSun_o(v_sun_o)
+    advitiy.setSun_b_m(v_sun_b)
+    advitiy.setLight(light)
+    
+    power_arr = pow.power(advitiy)
+    energy=energy+power_arr[6]*dt
+    power_output[i,0] = T[i]
+    power_output[i,1:7] = power_arr[0:6] #for each side:x,-x,y,-y,z,-z
+    power_output[i,7] = power_arr[6] #total power at t=T[i]
+    plt.plot(power_output[i,1],power_output[i,0])
+    plt.plot(power_output[i,2],power_output[i,0])
+    plt.plot(power_output[i,3],power_output[i,0])
+    plt.plot(power_output[i,4],power_output[i,0])
+    plt.plot(power_output[i,5],power_output[i,0])
+    plt.plot(power_output[i,6],power_output[i,0])
+power_output[0,8]=energy    
+np.savetxt("power_output.csv", power_output, delimiter=',')
