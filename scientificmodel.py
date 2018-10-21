059ff4f8804d800b7e2ceb5d51d201ced22a8313
Shritasingh <37498340+Shritasingh@users.noreply.github.com>
2018-10-21 10:10:46 +0530

    Update scientificmodel.py

--- scientificmodel.py
+++ scientificmodel.py
@@ -16,9 +16,9 @@ def sm_J2_mag(sat):
 
 def sm_gps_mag(sat): 
 	v_mag_i=sat.getMag_i()
-	J2data=sat.getgpsData()
-	v_pos_m=np.array([J2data[0],J2data[1],J2data[2]])
-	v_vel_m=np.array([J2data[3],J2data[4],J2data[5]])
+	gpsdata=sat.getgpsData()
+	v_pos_m=np.array([gpsdata[0],gpsdata[1],gpsdata[2]])
+	v_vel_m=np.array([gpsdata[3],gpsdata[4],gpsdata[5]])
 	v_mag_o=ecif2orbit(v_pos_m,v_vel_m,v_mag_i)
 	return v_mag_o
 
@@ -32,8 +32,8 @@ def sm_J2_sun(sat):
 
 def sm_gps_b(sat): 
 	v_sun_i=sat.getSun_i()
-	J2data=sat.getgpsData()
-	v_pos_m=np.array([J2data[0],J2data[1],J2data[2]])
-	v_vel_m=np.array([J2data[3],J2data[4],J2data[5]])
+	gpsdata=sat.getgpsData()
+	v_pos_m=np.array([gpsdata[0],gpsdata[1],gpsdata[2]])
+	v_vel_m=np.array([gpsdata[3],gpsdata[4],gpsdata[5]])
 	v_sun_o=ecif2orbit(v_pos_m,v_vel_m,v_sun_i)
 	return v_sun_o
